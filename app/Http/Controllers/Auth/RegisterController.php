5be2b09957e0399b96c941b9038f3965d832f6c3
Unknown <awc737@gmail.com>
2018-07-19 18:48:10 -0500

    Google oauth works on server
    Create user_accounts for related auth accounts

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Socialite;
 use App\User;
+use App\UserAccount;
 use App\Http\Controllers\Controller;
+/* TODO: why need?
+use Illuminate\Support\Facades\Auth; */
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-
-use Socialite;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -71,5 +73,84 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function createGoogleUser(array $data)
+    {
+        $userAccount = UserAccount::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'account_type' => $data['account_type'],
+            // 'user_id' => $userId,
+        ]);
+
+        // TODO: change to UserAccount::user_id?
+        $hasUser = User::where('email', $data['email'])->first();
+
+        // If user already exists, relate account and return
+        if($hasUser) {
+            $userAccount->user_id = $hasUser->id;
+            $userAccount->save();
+            return $hasUser;
+            // $this->guard()->login($hasUser);
+            // return redirect('/home');
+        }
+
+        // Else first time user
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            // 'username' => $data['name'],
+        ]);
+
+        $userAccount->user_id = $user->id;
+        $userAccount->save();
+
+        return $user;
+    }
+
+    /**
+     * Redirect the user to the Google authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->scopes('email')->stateless()->redirect();
+    }
+
+    /**
+     * Obtain the user information from Google.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $socialUser = Socialite::driver('google')->stateless()->user();
+        // $token = $user->token;
+
+        $emails = $socialUser->user['emails'];
+        $emailKey = array_search('accounts', array_column($emails, 'type'));
+        $data['name'] = $socialUser->user['displayName'];
+        $data['email'] = $emails[$emailKey]['value'];
+        $data['account_type'] = 'google';
+
+        // TODO: quick hack to log in first if account exists
+        $hasAccount = UserAccount::where('email', $data['email'])->where('account_type', 'google')->first();
+        if($hasAccount) {
+          $user = $hasAccount->user()->first();
+          $this->guard()->login($user);
+          return redirect('/');
+        }
+
+        event(new Registered($user = $this->createGoogleUser($data)));
+        $this->guard()->login($user);
+        return redirect('/');
     }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function user_accounts()
+    {
+      return $this->hasMany('App\UserAccount');
+    }
 }
